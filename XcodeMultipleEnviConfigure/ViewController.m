a641a78967acf0e24377dec27a6b261f0073ed2c
ASSASSINYM <darlingiloveu@sina.com>
2021-01-13 17:33:38 +0800

    项目配置pdf文档

--- XcodeMultipleEnviConfigure/ViewController.m
+++ XcodeMultipleEnviConfigure/ViewController.m
@@ -16,6 +16,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSString *host;
+#if TEST
+    host = @"http://www.test.com";
+#else
+    host = @"http://www.online.com";
+#endif
+    NSLog(@"%@",host);
 }
 
 
